a3287bf5e4bf2231fad6989a6337abbcaa8af6da
Tomasz Wojda <tomasz.wojda@wbdcontractor.com>
2026-08-12 23:39:29 +0200

    feat(shell): add dry-run flag to gcam

--- shell/shell.env.zsh
+++ shell/shell.env.zsh
@@ -1,6 +1,22 @@
 unalias gcam 2>/dev/null
 
 gcam() {
+  local dry_run=0
+
+  if [[ $# -gt 0 && ( "$1" == "-n" || "$1" == "--dry-run" ) ]]; then
+    dry_run=1
+    shift
+  fi
+
+  if [[ "$dry_run" == 1 ]]; then
+    if [[ $# -gt 0 ]]; then
+      print -r -- ">>> would run: git commit -am \"$*\""
+    else
+      print -r -- ">>> would run: git commit -am"
+    fi
+    return 0
+  fi
+
   if [[ $# -gt 0 ]]; then
     git commit -am "$*"
   else
