0500396de63c93a094ab29f83c1a7dbc39e7b537
Tomasz Wojda <tomasz.wojda@wbdcontractor.com>
2026-08-20 14:54:47 +0200

    fix(shell): add ls alias and improve gcam commit helper
    Add ls -la alias for directory listings. Require a commit message in gcam,
    add --amend support, and show usage instead of calling git commit -am without -m.

--- shell/shell.env.zsh
+++ shell/shell.env.zsh
@@ -1,27 +1,55 @@
-unalias gcam ssh 2>/dev/null
+unalias gcam ssh ls 2>/dev/null
 
+alias ls='ls -la'
 alias ssh='TERM=xterm ssh'
 
 gcam() {
   local dry_run=0
+  local amend=0
 
-  if [[ $# -gt 0 && ( "$1" == "-n" || "$1" == "--dry-run" ) ]]; then
-    dry_run=1
-    shift
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -n|--dry-run)
+        dry_run=1
+        shift
+        ;;
+      --amend)
+        amend=1
+        shift
+        ;;
+      --)
+        shift
+        break
+        ;;
+      -*)
+        print -u2 -r -- "gcam: unknown option: $1"
+        return 1
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+
+  if [[ $# -eq 0 ]]; then
+    print -u2 -r -- "usage: gcam [--amend] [--dry-run] <commit message>"
+    return 1
   fi
 
+  local msg="$*"
+
   if [[ "$dry_run" == 1 ]]; then
-    if [[ $# -gt 0 ]]; then
-      print -r -- ">>> would run: git commit -am \"$*\""
+    if [[ "$amend" == 1 ]]; then
+      print -r -- ">>> would run: git commit --amend -m \"$msg\""
     else
-      print -r -- ">>> would run: git commit -am"
+      print -r -- ">>> would run: git commit -am \"$msg\""
     fi
     return 0
   fi
 
-  if [[ $# -gt 0 ]]; then
-    git commit -am "$*"
+  if [[ "$amend" == 1 ]]; then
+    git commit --amend -m "$msg"
   else
-    git commit -am
+    git commit -am "$msg"
   fi
 }
